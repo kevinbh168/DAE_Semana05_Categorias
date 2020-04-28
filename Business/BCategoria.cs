b6abb17374c226daf5254ef0694e498c2a53489a
Kevin Angello Breña Huachin <kevin.brena@tecsup.edu.pe>
2020-04-28 02:24:01 -0500

    lab terminado categoria

---
+++ Business/BCategoria.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+namespace Business
+{
+    public class BCategoria
+    {
+        private DCategoria dCategoria = null;
+
+        public List<Categoria> Listar(int IdCategoria)
+        {
+            List<Categoria> categorias = null;
+            try
+            {
+                dCategoria = new DCategoria();
+                categorias = dCategoria.Listar(new Categoria { IdCategoria = IdCategoria });
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            return categorias;
+        }
+
+        public bool Insertar(Categoria categoria)
+        {
+            bool result = true;
+            try
+            {
+                dCategoria = new DCategoria();
+                int ultimoId= dCategoria.ObtenerUltimoId();
+                if (ultimoId != -1)
+                {
+                    categoria.IdCategoria = ultimoId;
+                    dCategoria.Insertar(categoria);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public bool Actualizar(Categoria categoria)
+        {
+            bool result = true;
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Actualizar(categoria);
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public bool Eliminar(int IdCategoria)
+        {
+            bool result = true;
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Eliminar(IdCategoria);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
---
+++ Semana05/ListaCategoria.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        public ListaCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+        private void Cargar()
+        {
+            BCategoria bCategoria = null;
+            try
+            {
+                bCategoria = new BCategoria();
+                dgvCategoria.ItemsSource = bCategoria.Listar(0);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                bCategoria = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            ManCategoria man = new ManCategoria(0);
+            man.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)dgvCategoria.SelectedItem;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+            ManCategoria man = new ManCategoria(idCategoria);
+            man.ShowDialog();
+            Cargar();
+        }
+    }
+}
